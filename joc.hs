15f792a8e359d720003b639745b59a9ac92f57f6
francesc <96salarfrancesc@gmail.com>
2020-04-26 19:03:47 +0200

    Uses 2d list instead of map

--- joc.hs
+++ joc.hs
@@ -7,7 +7,6 @@ import System.IO (BufferMode(..), stdin, stdout, hReady, hSetBuffering, hSetEcho
 import Data.Ord (comparing)
 import Data.List (groupBy, maximumBy, sortOn, intersperse)
 import Data.Maybe (fromJust)
-import Data.Map.Strict  (Map, fromList, (!), insertWith) 
 import Control.Monad (forM_)
 
 ---------- TYPE DECLARATIONS -----------
@@ -15,17 +14,17 @@ newtype Strategy = Strategy { runStrategy :: Board -> Player -> IO Int }
 
 data Player = Red | Yellow deriving (Eq, Show)
 
-data Board = Board 
+data Board = Board
   { boardSize    :: (Int, Int)
-  , boardColumns :: Map Int [Player] }
+  , boardColumns :: [[Player]] }
 
 data Game = Game
   { gameBoard      :: Board
   , strategyRed    :: Strategy
-  , strategyYellow :: Strategy 
+  , strategyYellow :: Strategy
   , currentTurn    :: Player }
 
-data GameCommand = GCSetup | GCPlay | GCExit  
+data GameCommand = GCSetup | GCPlay | GCExit
 
 data Key = KeyLeft | KeyRight | KeyEnter deriving Show
 
@@ -41,31 +40,31 @@ colorEmpty  = ANSI.xterm6LevelRGB 1 1 1 -- Grey
 colorRed    = ANSI.xterm6LevelRGB 5 0 0 -- Red
 colorYellow = ANSI.xterm6LevelRGB 5 5 0 -- Yellow
 
-defaultGame = Game 
-  { gameBoard      = getEmptyBoard 7 6
+defaultGame = Game
+  { gameBoard      = getEmptyBoard 5 4
   , strategyRed    = humanS
   , strategyYellow = humanS
-  , currentTurn    = Red } 
+  , currentTurn    = Red }
 
-colorForPlayer = \case 
+colorForPlayer = \case
   Red    -> colorRed
   Yellow -> colorYellow
 
 ------------ Strategies -------------------
 randomS :: Strategy
 randomS = Strategy $ \board _ -> randomPick (getAvailableCols board)
-    where randomPick xs = do 
+    where randomPick xs = do
             random <- randomIO :: IO Int
-            let index = random `mod` length xs
-            return $ xs!!index
+            let i = random `mod` length xs
+            return $ xs !! i
 
 greedyS :: Strategy
-greedyS = Strategy $ 
-  \board player -> 
+greedyS = Strategy $
+  \board player ->
     let rival = otherPlayer player
         greedyHeuristic col = case (scorePlayer player, scorePlayer rival) of
             (4, _) -> 5 -- player can connect 4 --> score 5
-            (_, 4) -> 4 -- rival can connect 4  --> score 4 
+            (_, 4) -> 4 -- rival can connect 4  --> score 4
             (l, _) -> l -- player can connect l --> score (3, 2, 1)
             where scorePlayer p = length (getLongestLine board p col)
     in return . maximumBy (comparing greedyHeuristic) . getAvailableCols $ board
@@ -74,13 +73,13 @@ smartS :: Strategy
 smartS = Strategy $ error "TODO"
 
 humanS :: Strategy
-humanS = Strategy $ 
+humanS = Strategy $
   \board p ->
-    let validCols = getAvailableCols board  
+    let validCols = getAvailableCols board
         (nCols, _) = boardSize board
         readValidColumn = do
-          col <- columnSelector p (1, nCols)
-          if col `elem` validCols 
+          col <- columnSelector p (0, nCols - 1)
+          if col `elem` validCols
             then return col
             else readValidColumn
     in readValidColumn
@@ -92,27 +91,27 @@ humanS = Strategy $
 ----------- BOARD UTILS ---------------
 getEmptyBoard :: Int -> Int -> Board
 getEmptyBoard nCols nRows = Board {boardSize = (nCols, nRows), boardColumns = empty}
-  where empty = fromList $ [1..nCols] `zip` repeat []
+  where empty = replicate nCols []
 
 getAvailableCols :: Board -> [Int]
-getAvailableCols Board {boardSize = (nCols, nRows), boardColumns = columns} = 
-  filter available [1..nCols] 
-    where available i = length (columns ! i) < nRows
+getAvailableCols Board {boardSize = (nCols, nRows), boardColumns = columns} =
+  filter available [0..nCols-1]
+    where available i = length (columns !! i) < nRows
 
 getLongestLine :: Board -> Player -> Int -> [(Int, Int)]
 getLongestLine b player col =
   let (nCols, nRows) = boardSize b
       columns = boardColumns b
-      row = length (columns ! col) + 1
+      row = length (columns !! col)
       count = takeWhile (isPlayer . getPlayerAt b)
         where isPlayer Nothing  = False
               isPlayer (Just p) = p == player
 
-      l = [col-1, col-2..1]
-      r = [col+1..nCols]
-      u = [row+1..nRows]
-      d = [row-1, row-2..1]
-      
+      l = [col-1, col-2..0]
+      r = [col+1..nCols-1]
+      u = [row+1..nRows-1]
+      d = [row-1, row-2..0]
+
       x = repeat row
       y = repeat col
 
@@ -124,14 +123,14 @@ getLongestLine b player col =
   in maximumBy (comparing length) [ vert, hor, diag1, diag2]
 
 getPlayerAt :: Board -> (Int, Int) -> Maybe Player
-getPlayerAt Board {boardSize = (nCols, _), boardColumns = columns} (col, row) = 
-  if inside 
-    then Just $ columns ! col !! (row-1)
+getPlayerAt Board {boardSize = (nCols, _), boardColumns = columns} (col, row) =
+  if inside
+    then Just $ columns !! col !! row
     else Nothing
-  where inside = col >= 1 && col <= nCols && row >= 1 && row <= length (columns ! col) 
+  where inside = col >= 0 && col < nCols && row >= 0 && row < length (columns !! col)
 
 playMove :: Board -> Player -> Int -> Board
-playMove b p c = b { boardColumns = insertWith (flip (++)) c [p] (boardColumns b) }
+playMove b p c = b { boardColumns = mapIth c (++ [p]) (boardColumns b) }
 
 otherPlayer :: Player -> Player
 otherPlayer = \case
@@ -142,20 +141,31 @@ otherPlayer = \case
 main :: IO ()
 main = initGame >> setupGame >>= loopGame
   where loopGame g =
-          runGame g >> endGame >>= \case 
+          runGame g >>= endGame >>= \case
                GCPlay  -> loopGame g
                GCSetup -> main
                GCExit  -> quitGame
-  
+
 initGame :: IO ()
 initGame = ANSI.clearScreen >> ANSI.hideCursor
 
 quitGame :: IO ()
 quitGame = ANSI.showCursor
 
-endGame :: IO GameCommand
-endGame = do
-  ANSI.setCursorPosition 0 0
+endGame :: (Game, Maybe Player) -> IO GameCommand
+endGame (game, winner) = do
+  let height = (+2) . snd . boardSize. gameBoard $ game
+  ANSI.setCursorPosition height 0
+  case winner of 
+    Nothing -> putStr "Game drawn.."
+    Just p -> do
+        putStr "Player "
+        ANSI.setSGR [ANSI.SetPaletteColor ANSI.Foreground (colorForPlayer p)]
+        putStr (show p)
+        ANSI.setSGR [ANSI.Reset]
+        putStr " won! "
+        hFlush stdout
+  ANSI.setCursorPosition (height+1) 0
   menuSelector [ (GCPlay , "Play again")
                , (GCSetup, "Options")
                , (GCExit , "Exit") ]
@@ -163,24 +173,22 @@ endGame = do
 setupGame :: IO Game
 setupGame = return defaultGame
 
-runGame :: Game -> IO (Maybe Player)
-runGame game = do
-  let board = gameBoard game
+runGame :: Game -> IO (Game, Maybe Player)
+runGame  game@Game{ gameBoard = board, currentTurn = player} = do
   drawBoard board
   if null (getAvailableCols board)
-    then return Nothing -- No more moves available -> Game drawn
+    then return (game, Nothing) -- No more moves available -> Game drawn
     else do
-      let player   = currentTurn game
-          strategy = case player of 
+      let strategy = case player of
             Yellow -> strategyYellow game
             Red    -> strategyRed game
       column <- runStrategy strategy board player
       let line = getLongestLine board player column
           board' = playMove board player column
-      if length line >= 4
-        then drawWinningLine board' line >> return (Just player)
-        else runGame game { gameBoard = board', currentTurn = otherPlayer player }
- 
+      if length line < 4
+        then runGame game { gameBoard = board', currentTurn = otherPlayer player }
+        else drawWinningLine board' line >> return (game, Just player)
+
 drawBoard :: Board -> IO ()
 drawBoard board@Board{boardSize = (nCols, nRows), boardColumns = columns} = do
   -- Draw the frame
@@ -192,25 +200,25 @@ drawBoard board@Board{boardSize = (nCols, nRows), boardColumns = columns} = do
     ANSI.cursorForward nCols
     putStr "│"
   -- Draw the board
-  forM_ ( (,) <$> [1..nCols] <*> [1..nRows] ) $ \(c, r) ->  do
-    ANSI.setCursorPosition (y0 + nRows - r + 1) (x0 + c)
+  forM_ ( (,) <$> [0..nCols-1] <*> [0..nRows-1] ) $ \(c, r) ->  do
+    ANSI.setCursorPosition (y0 + nRows - r) (x0 + c + 1)
     let (color, char) = case getPlayerAt board (c, r) of
                           Nothing -> (colorEmpty, charEmpty)
                           Just p  -> (colorForPlayer p, charPiece)
     ANSI.setSGR [ANSI.SetPaletteColor ANSI.Foreground color]
     putChar char
-    ANSI.setSGR [] 
-  hFlush stdout 
-  
+    ANSI.setSGR []
+  hFlush stdout
+
 drawWinningLine :: Board -> [(Int, Int)] -> IO ()
 drawWinningLine board@Board{boardSize = (nCols, nRows), boardColumns = columns} line = do
-  forM_ line $ \(c, r) ->  do
-    ANSI.setCursorPosition (y0 + nRows - r + 1) (x0 + c)
+  forM_ line $ \(c, r) -> do
     let color = colorForPlayer . fromJust . getPlayerAt board $ (c, r)
+    ANSI.setCursorPosition (y0 + nRows - r) (x0 + c + 1)
     ANSI.setSGR [ANSI.SetPaletteColor ANSI.Foreground color]
     putChar charWin
-    ANSI.setSGR [] 
-  hFlush stdout 
+    ANSI.setSGR [ANSI.Reset]
+  hFlush stdout
 
 
 menuSelector :: [(a, String)] -> IO a
@@ -223,23 +231,23 @@ menuSelector menu = do
             ANSI.cursorBackward lengthMenu
             hFlush stdout
           highlight a = ANSI.setSGR [ANSI.SetSwapForegroundBackground True] >> a >> ANSI.setSGR [ANSI.Reset]
-          separate = intersperse (putStr separator) 
+          separate = intersperse (putStr separator)
           separator = "  |  "
           lengthMenu = length separator * (length (foldMap snd menu) - 1)
 
 columnSelector :: Player -> (Int, Int) -> IO Int
 columnSelector player (l, r) = do
   ANSI.setCursorPosition x0 y0
-  i <- selectorController (l, r) printF ((r+l) `quot` 2)
+  i <- selectorController (l, r) printF ((l + r) `quot` 2)
   ANSI.clearLine
   return i
     where printF i = do
             ANSI.clearLine
-            ANSI.cursorForward i 
+            ANSI.cursorForward (i+1)
             ANSI.setSGR [ANSI.SetPaletteColor ANSI.Foreground (colorForPlayer player)]
             putChar charPiece
-            ANSI.setSGR []
-            ANSI.cursorBackward (i+1)
+            ANSI.setSGR [ANSI.Reset]
+            ANSI.cursorBackward (i+2)
             hFlush stdout
 
 selectorController :: (Int, Int) -> (Int -> IO ()) -> Int -> IO Int
@@ -247,9 +255,8 @@ selectorController (l, r) paintF i0 = go i0
   where go i = paintF i >> getKey >>= \case
             Just KeyRight -> if i < r then go (i+1) else go i
             Just KeyLeft  -> if l < i then go (i-1) else go i
-            Just KeyEnter -> ANSI.cursorBackward 1 >> return i
+            Just KeyEnter -> return i
             Nothing       -> go i
-
       --getKey :: IO (Maybe Key)
         getKey = do
           hSetEcho stdout False
@@ -258,18 +265,17 @@ selectorController (l, r) paintF i0 = go i0
             '\ESC' -> parseArrowKey
             '\n'   -> return $ Just KeyEnter
             _  -> getKey
-        
       --parseArrowKey :: IO (Maybe Key)
         parseArrowKey =
-          getChar >>= \case 
+          getChar >>= \case
             '\ESC' -> parseArrowKey
             '['    -> getChar >>= \case
                   'C' -> return $ Just KeyRight
                   'D' -> return $ Just KeyLeft
                   _   -> return Nothing
             _   -> return Nothing
-            
+
 mapIth :: Int -> (a -> a) -> [a] -> [a]
-mapIth i f xs = 
+mapIth i f xs =
   let (a, b:c) = splitAt i xs
   in a ++ f b : c
